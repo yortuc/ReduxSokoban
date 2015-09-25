675300f873e588d2a162355236f71f8f8a8eb26a
Evren Yortuçboylu <evox@Evrens-MacBook-Pro.local>
2015-09-26 01:24:04 +0300

    static server

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+	http = require('http'),
+	app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// static files
+app.use("/", express.static(__dirname + "/dist"));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var port = process.env.PORT || 8100;
+app.listen(port);
+console.log('listening on port :' + port);
