c27f87cda6dcccdb4edf0b0b1ca391c80b8239aa
Evren Yortuçboylu <evox@Evrens-MacBook-Pro.local>
2015-09-26 01:14:22 +0300

    static server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express'); 
+var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 3000);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/dist'));
